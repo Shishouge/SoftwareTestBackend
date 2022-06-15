46f60c02afccacf5421b5bd7773a1fdfcfbe39e2
Han-tj <1053695217@qq.com>
2022-06-15 23:10:14 +0800

    finish comSale

--- src/main/java/com/example/backend/Controller/SaleCom/SaleComController.java
+++ src/main/java/com/example/backend/Controller/SaleCom/SaleComController.java
@@ -1,22 +1,32 @@
 package com.example.backend.Controller.SaleCom;
 
+import com.alibaba.fastjson.JSON;
+import com.example.backend.Entity.SaleComCase;
+import com.example.backend.Entity.TriangleCase;
 import com.example.backend.Service.SaleComTest.SaleComTestService;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.List;
 
 @CrossOrigin
 @Controller
 public class SaleComController {
     @Autowired
     SaleComTestService t;
-    @RequestMapping(value = "/getSaleComTestResult",method = RequestMethod.GET)
+    @RequestMapping(value = "/saleCom/SaleComTestResult",method = RequestMethod.POST)
     @ResponseBody
-    public JSONObject getSaleComTestResult(){
-        return t.testStaticFile();
+    public JSONObject getSaleComTestResult(@RequestBody String jsonStr)throws UnsupportedEncodingException {
+        String urlStr = URLDecoder.decode(jsonStr, "UTF-8");
+        System.out.println(urlStr.substring(0,urlStr.length()-1));
+        // 传过来的json字符串格式会乱加东西 所以要截取
+        List<SaleComCase> list = JSON.parseArray(urlStr.substring(0,urlStr.length()-1), SaleComCase.class);
+        System.out.println(list);
+
+        return t.testCaseBatch(list);
     }
 }
--- src/main/java/com/example/backend/Service/SaleComTest/SaleComTestService.java
+++ src/main/java/com/example/backend/Service/SaleComTest/SaleComTestService.java
@@ -1,5 +1,6 @@
 package com.example.backend.Service.SaleComTest;
 
+import com.example.backend.Entity.SaleComCase;
 import com.example.backend.Solution.Calendar.returnformat;
 import com.example.backend.Solution.SaleCom.SaleCom;
 import jxl.Sheet;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +32,60 @@ public class SaleComTestService {
         this.json = new JSONObject();
         this.errorList = new ArrayList<>();
     }
+
+    public JSONObject testCaseBatch(List<SaleComCase> saleComCaseList){
+        int right_num=0;
+        int error_num=0;
+        JSONObject totalResult= new JSONObject();
+        List<JSONObject> resultList=new ArrayList<>();
+        List<JSONObject> error_info=new ArrayList<>();
+
+        for(int i =0;i<saleComCaseList.size();i++){
+            JSONObject returnedjson =new JSONObject();
+            String actual= sc.checkReward(
+                    (int)saleComCaseList.get(i).getMainUnit(),(int)saleComCaseList.get(i).getScreen(),(int)saleComCaseList.get(i).getPerpheral()
+            );
+            SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String testTime = sdf.format(new java.util.Date());
+
+            returnedjson.put("id",saleComCaseList.get(i).getId());
+            returnedjson.put("mainUnit", saleComCaseList.get(i).getMainUnit());
+            returnedjson.put("screen", saleComCaseList.get(i).getScreen());
+            returnedjson.put("perpheral", saleComCaseList.get(i).getPerpheral());
+            returnedjson.put("expectedOutput",saleComCaseList.get(i).getExpectedOutput());
+            returnedjson.put("actualOutput",actual);
+            returnedjson.put("info","Success");
+            returnedjson.put("testTime",testTime);
+
+            if(actual.equals(saleComCaseList.get(i).getExpectedOutput())){
+                returnedjson.put("testResult","Pass");
+                right_num++;
+            }else{
+                returnedjson.put("testResult","Error");
+                error_info.add(returnedjson);
+                error_num++;
+            }
+
+            resultList.add(returnedjson);
+
+        }
+        List<JSONObject> pieData=new ArrayList<>();
+        JSONObject temp=new JSONObject();
+        temp.put("value",right_num);
+        temp.put("name","right");
+        pieData.add(temp);
+        JSONObject temp1=new JSONObject();
+        temp1.put("value",error_num);
+        temp1.put("name","error");
+        pieData.add(temp1);
+
+        totalResult.put("result_list",resultList);
+        totalResult.put("pieData",pieData);
+        totalResult.put("error_info",error_info);
+
+        return totalResult;
+    }
+
     public JSONObject testStaticFile(){
         String address = "src/main/resources/static/SaleComTestData/SaleComTest1.xls";
         File file = new File(address);
